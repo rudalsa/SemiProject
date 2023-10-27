7ebb8ae679ba0869b50f31aed0aa020b2ddc6716
chyukl <chaehyuk97@gmail.com>
2023-10-27 10:41:45 +0900

    commonproperties 경로 변경, CarinfoAction을 WowcarinfoAction으로 변경, uri 매핑 다시했음

--- SemiProject/src/main/java/game/controller/WowcarinfoAction.java
+++ SemiProject/src/main/java/game/controller/WowcarinfoAction.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class CarinfoAction extends AbstractController {
+public class WowcarinfoAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		super.setRedirect(false);
-	  	super.setViewPage("/WEB-INF/shop/carinfo.jsp");
+	  	super.setViewPage("/WEB-INF/shop/wowcarinfo.jsp");
 		
 	}
 	
