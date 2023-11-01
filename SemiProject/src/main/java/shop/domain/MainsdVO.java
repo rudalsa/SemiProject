23474151827577d34ef3fb8b54928fdf4eddb5b5
SIST <SIST@DESKTOP-2OOVOKA>
2023-11-01 18:12:06 +0900

    백업파일 재업로드

--- SemiProject/src/main/java/shop/domain/MainsdVO.java
+++ SemiProject/src/main/java/shop/domain/MainsdVO.java
@@ -5,7 +5,7 @@ public class MainsdVO {
 	private int ms_img_no; 	   		// 카테고리 등록순서
 	private String fk_g_code;  		// 메인슬라이드 이미지 해당 게임 코드
 	private String ms_bg_file; 		// 메인슬라이드 이미지 파일 
-	private String ms_logo_file;   // 메인슬라이드 로고 이미지 파일
+	private String ms_logo_file;    //메인슬라이드 로고 이미지 파일
 	
 	
 	public int getMs_img_no() {
